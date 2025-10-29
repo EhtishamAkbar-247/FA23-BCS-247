4c9008ef509d41ca1ce993a5200618ecd5c93864
Ehtisham Akbar <madpcgamerz766@gmail.com>
2025-10-29 22:23:02 +0500

    Initialize main.dart for Flutter application

---
+++ LAB MID/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:mess_pos/app.dart'; // This import is already correct.
+
+// --- MAIN APPLICATION SETUP ---
+
+void main() {
+  runApp(const MyApp());
+}
